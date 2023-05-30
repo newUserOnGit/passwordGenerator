451da51667cc9af3a202a1feb472d5e8bb08593d
newUserOnGit <kondrateffal@yandex.ru>
2023-05-30 14:48:30 +0600

    refactor function read_user_settings

--- venv/PasswordGenerator.py
+++ venv/PasswordGenerator.py
@@ -10,32 +10,21 @@ class PasswordGenerator():
     list_password = []
     while True:
         try:
-            pwd_length = int(input('Укаэите длину необходимого пароля: '))
-            pwd_digith = input(f'В пароле должны присутствовать цифры? (Y - да, N - нет)')
-            pwd_uppercase = input(f'В пароле должны присутствовать заглавные буквы? (Y - да, N - нет)')
-            pwd_lowercase = input(f'В пароле должны присутствовать маленькие буквы? (Y - да, N - нет)')
-            pwd_punctuation = input(f'В пароле должны присутствовать символы? (Y - да, N - нет)')
+            pwd_length = int(input(
+                'Укаэите длину необходимого пароля: '))
+            pwd_digith = True if input(
+                'В пароле должны присутствовать цифры? (Y - да, N - нет)') == 'y' else False
+            pwd_uppercase = True if input(
+                'В пароле должны присутствовать заглавные буквы? (Y - да, N - нет)') == 'y' else False
+            pwd_lowercase = True if input(
+                'В пароле должны присутствовать маленькие буквы? (Y - да, N - нет)') == 'y' else False
+            pwd_punctuation = True if input(
+                'В пароле должны присутствовать символы? (Y - да, N - нет)') == 'y26' else False
         except ValueError:
             continue
         break
 
-    def read_user_settings(self):
-        if self.pwd_digith == 'y':
-            self.pwd_digith = True
-        else:
-            self.pwd_digith = False
-        if self.pwd_uppercase == 'y':
-            self.pwd_uppercase = True
-        else:
-            self.pwd_uppercase = False
-        if self.pwd_lowercase == 'y':
-            self.pwd_lowercase = True
-        else:
-            self.pwd_lowercase = False
-        if self.pwd_punctuation == 'y':
-            self.pwd_punctuation = True
-        else:
-            self.pwd_punctuation = False
+
 
     def generation_symbosl(self):
         if self.pwd_digith:
@@ -53,12 +42,11 @@ class PasswordGenerator():
     def create_password(self):
         list_password = numpy.array(self.list_password)
         self.ready_password = ''.join(random.choice(list_password) for i in range(0, self.pwd_length))
-        print(self.ready_password)
 
     def main(self):
-        self.read_user_settings()
         self.generation_symbosl()
         self.create_password()
+        print(self.ready_password)
 
 if __name__ == '__main__':
     start = PasswordGenerator()
